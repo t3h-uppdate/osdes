c7ee88e1893d8d07266a8008b19783e12a42a07b
t3h-uppdate <olarsson000@gmail.com>
2025-04-13 16:24:25 +0000

    Fix top margin of the Herosection

--- src/features/hero/components/HeroSection.tsx
+++ src/features/hero/components/HeroSection.tsx
@@ -9,7 +9,8 @@ function HeroSection() {
     <section id="home" className="relative overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          {/* Increased base margin-top from mt-10 to mt-20 */}
+          <main className="mt-20 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
                 {/* Use t function for translatable text */}
